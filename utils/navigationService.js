3d9ac0663afee7098675800c45cc75b77529f224
Sophie <bolesalavb@gmail.com>
2021-06-29 03:26:38 -0700

    Add NavigationService

---
+++ utils/navigationService.js
@@ -0,0 +1,15 @@
+import { NavigationActions } from 'react-navigation';
+
+let _navigator;
+
+const setTopLevelNavigator = (navigatorRef) => {
+    _navigator = navigatorRef;
+}
+
+const navigate = (routeName, params) => {
+    _navigator.dispatch(
+        NavigationActions.navigate({ routeName, params })
+    );
+}
+
+export default { navigate, setTopLevelNavigator };
