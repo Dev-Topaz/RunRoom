4a698c3a7d4649fbe946fa5c834e06172f9455df
Sophie <bolesalavb@gmail.com>
2021-07-06 10:32:56 -0700

    Add Profile screen

---
+++ screens/profile/profile.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text, Pressable, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import global from '../../global';
+import css from '../../css';
+
+import { useSelector } from 'react-redux';
+
+const TopTab = createMaterialTopTabNavigator();
+
+const Profile = (props) => {
+
+    const userId = useSelector(state => state.user.userId);
+    const accessToken = useSelector(state => state.user.accessToken);
+    const [userInfo, setUserInfo] = useState({});
+
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={styles.header}>
+                <Text style={[css.titleText, { color: global.COLOR.PRIMARY100, marginBottom: 10 }]}>PROFILE</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Image source={} style={css.hostAvatar}/>
+                    <View style={[css.hostInfo, { justifyContent: 'center' }]}>
+                        <Text style={[css.hostName, { color: global.COLOR.PRIMARY100 }]}></Text>
+                        <Text style={[css.hostLabel, { marginTop: 2 }]}></Text>
+                    </View>
+                    <Pressable style={[css.inviteButton, { backgroundColor: global.COLOR.BACKGROUND }]}>
+                        <Text style={css.inviteButtonText}>Edit Profile</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View style={{ flex: 1 }}>
+                <NavigationContainer>
+                    <TopTab.Navigator
+                        initialRouteName='ProfileFinished'
+                        tabBarOptions={{
+                            activeTintColor: global.COLOR.PRIMARY100,
+                            inactiveTintColor: global.COLOR.PRIMARY70,
+                            labelStyle: styles.tabText,
+                            style: { backgroundColor: 'white', paddingVertical: 14 },
+                            indicatorStyle: { color: global.COLOR.PRIMARY100 }
+                        }}
+                        backBehavior='none'
+                    >
+                        <TopTab.Screen
+                            name='ProfileFinished'
+                            component={ProfileFinished}
+                            options={{ tabBarLabel: 'Finished Runs' }}
+                        />
+                        <TopTab.Screen
+                            name='ProfileStatistics'
+                            component={ProfileStatistics}
+                            options={{ tabBarLabel: 'Statistics' }}
+                        />
+                        <TopTab.Screen
+                            name='ProfileConnection'
+                            component={ProfileConnection}
+                            options={{ tabBarLabel: 'Connections' }}
+                        />
+                    </TopTab.Navigator>
+                </NavigationContainer>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: global.CONSTANTS.SPACE_55,
+        paddingHorizontal: global.CONSTANTS.SIZE_20,
+        paddingBottom: 12,
+    },
+    tabText: {
+        fontFamily: 'SFProMedium',
+        fontSize: 14,
+        textTransform: 'none',
+    },
+});
+
+export default Profile;
