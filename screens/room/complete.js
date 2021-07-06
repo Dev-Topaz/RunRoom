13cfec3d54d69030a52d538bcac19c2db09d9929
Sophie <bolesalavb@gmail.com>
2021-07-05 19:34:00 -0700

    Add RoomComplete screen

---
+++ screens/room/complete.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import SvgIcon from '../../components/svgIcon';
+import global from '../../global';
+
+
+const RoomComplete = (props) => {
+
+    const [remainSec, setRemainSec] = useState(3);
+
+    useEffect(() => {
+        const timer = setInterval(() => setRemainSec(remainSec => remainSec - 1), 1000);
+        if(remainSec < 1) {
+            clearInterval(timer);
+            props.navigation.navigate('Main');
+        }
+        return () => clearInterval(timer);
+    }, [remainSec]);
+
+    return (
+        <View style={styles.bgContainer}>
+            <SvgIcon icon='CheckLogo'/>
+            <Text style={styles.text}>RUNROOM CREATED</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    bgContainer: {
+        flex: 1,
+        backgroundColor: global.COLOR.PRIMARY100,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontFamily: 'FuturaT',
+        fontSize: 24,
+        color: 'white',
+        marginTop: 25,
+    },
+});
+
+export default RoomComplete;
