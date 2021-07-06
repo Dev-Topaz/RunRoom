9acf64be9cf72c594fbd3a3f6fa14b6c5ae12083
Sophie <bolesalavb@gmail.com>
2021-07-06 12:24:47 -0700

    Fix some issues in EditProfile

--- screens/profile/edit.js
+++ screens/profile/edit.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image, Text, Pressable, TouchableOpacity, TextInput, Modal, Alert } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import SwitchToggle from 'react-native-switch-toggle';
+import PhoneInput from 'react-native-phone-input';
+import CountryPicker from '../../components/countryPicker';
+import SvgIcon from '../../components/svgIcon';
+import global from '../../global';
+import css from '../../css';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getUserDetails } from '../../utils/api';
+import { getUserDetails, updateUserProfile } from '../../utils/api';
 
 const EditProfile = (props) => {
 
@@ -11,6 +18,44 @@ const EditProfile = (props) => {
     const phoneNumber = useSelector(state => state.user.phoneNumber);
     const dispatch = useDispatch();
     const phoneInput = useRef(null);
+
+    const [name, setName] = useState({ firstName: '', lastName: '' });
+    const [location, setLocation] = useState('');
+    const [ageGroup, setAgeGroup] = useState(0);
+    const [gender, setGender] = useState(0);
+    const [avatar, setAvatar] = useState(null);
+    const [isToggle, setToggle] = useState(false);
+    const [countryData, setCountryData] = useState([]);
+
+    const [countryVisible, setCountryVisible] = useState(false);
+    const [alertVisible, setAlertVisible] = useState(false);
+    const [ageVisible, setAgeVisible] = useState(false);
+    const [genderVisible, setGenderVisible] = useState(false);
+
+    useEffect(() => {
+        setCountryData(phoneInput.current.getPickerData());
+        getUserDetails(userId, accessToken).then(result => {
+            if(result == null) {
+                Alert.alert('You are not authorized.');
+            } else if(userId != result.userId || phoneNumber != result.phoneNumber) {
+                Alert.alert('You are a fake member.');
+            } else {
+                setName({ firstName: result.firstName, lastName: result.lastName });
+                setLocation(result.location);
+                setAgeGroup(result.ageGroup);
+                setAvatar(result.avatar);
+                setGender(result.gender);
+            }
+        });
+    }, []);
+
+    const selectCountry = (item) => {
+        phoneInput.current.selectCountry(item.iso2);
+    }
+
+    const pressSubmitAction = () => {
+
+    }
 
     return (
         <View></View>
--- utils/api.js
+++ utils/api.js
@@ -333,3 +333,34 @@ export async function stopFollowing(followingId, accessToken) {
 
     return result;
 }
+
+export async function updateUserProfile(updateInfo, accessToken) {
+
+    const result = Axios.put('/Users/UpdateUserProfile', {
+        UserId: updateInfo.userId,
+        FirstName: updateInfo.firstName,
+        LastName: updateInfo.lastName,
+        ImageInfo: updateInfo.avatar,
+        RunningLocation: updateInfo.location,
+        Gender: updateInfo.gender,
+        AgeGroup: updateInfo.ageGroup,
+        UnitOfMeasurement: updateInfo.unit,
+        Email: '',
+    }, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    }).then(
+        async function(response) {
+            if(response.status == 200)
+                return response.data.success;
+            else
+                return false;
+        }
+    ).catch(err => {
+        console.log(err);
+        return false;
+    });
+
+    return result;
+}
