c2f5900fdb7ef8da5c87158a14d4e2b1a8df6fef
Sophie <bolesalavb@gmail.com>
2021-07-06 19:36:43 -0700

    Fix an issue in AgePicker

--- components/agePicker.js
+++ components/agePicker.js
@@ -8,6 +8,19 @@ const AgePicker = (props) => {
 
     const [ageGroup, setAgeGroup] = useState(0);
 
+    const pressGroupButton = (index) => {
+        if(index == ageGroup) {
+            setAgeGroup(0);
+        } else {
+            setAgeGroup(index);
+        }
+    }
+
+    const pressSubmitAction = () => {
+        props.onChangeValue(ageGroup);
+        props.onChangeVisible(false);
+    }
+
     return (
         <Modal
             animationType='slide'
@@ -19,46 +32,49 @@ const AgePicker = (props) => {
             <View style={css.overlay}>
                 <View style={styles.modalContainer}>
                     <Text style={css.modalTitleText}>Select your age group</Text>
+                    <Pressable style={styles.closeButton} onPress={() => props.onChangeVisible(false)}>
+                        <SvgIcon icon='Close'/>
+                    </Pressable>
                     <View style={styles.buttonGroup}>
                         <View style={styles.column}>
-                            <Pressable style={ageGroup == 1 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 1 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(1)}>
                                 <Text style={ageGroup == 1 ? styles.activeText : styles.inactiveText}>Below 20</Text>
                                 { ageGroup == 1 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
-                            <Pressable style={ageGroup == 3 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 3 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(3)}>
                                 <Text style={ageGroup == 3 ? styles.activeText : styles.inactiveText}>30s</Text>
                                 { ageGroup == 3 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
-                            <Pressable style={ageGroup == 5 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 5 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(5)}>
                                 <Text style={ageGroup == 5 ? styles.activeText : styles.inactiveText}>50s</Text>
                                 { ageGroup == 5 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
-                            <Pressable style={ageGroup == 7 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 7 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(7)}>
                                 <Text style={ageGroup == 7 ? styles.activeText : styles.inactiveText}>70s</Text>
                                 { ageGroup == 7 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
                         </View>
                         <View style={styles.column}>
-                            <Pressable style={ageGroup == 2 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 2 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(2)}>
                                 <Text style={ageGroup == 2 ? styles.activeText : styles.inactiveText}>20s</Text>
                                 { ageGroup == 2 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
-                            <Pressable style={ageGroup == 4 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 4 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(4)}>
                                 <Text style={ageGroup == 4 ? styles.activeText : styles.inactiveText}>40s</Text>
                                 { ageGroup == 4 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
-                            <Pressable style={ageGroup == 6 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 6 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(6)}>
                                 <Text style={ageGroup == 6 ? styles.activeText : styles.inactiveText}>60s</Text>
                                 { ageGroup == 6 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
-                            <Pressable style={ageGroup == 8 ? styles.groupActiveButton : styles.groupInactiveButton}>
+                            <Pressable style={ageGroup == 8 ? styles.groupActiveButton : styles.groupInactiveButton} onPress={() => pressGroupButton(8)}>
                                 <Text style={ageGroup == 8 ? styles.activeText : styles.inactiveText}>80 or above</Text>
                                 { ageGroup == 8 ? <View style={{position: 'absolute', left: 20}}><SvgIcon icon='SmallCheck'/></View> : null }
                             </Pressable>
                         </View>
                     </View>
                     <View style={styles.footer}>
-                        <TouchableOpacity style={css.submitButton}>
+                        <TouchableOpacity style={css.submitButton} onPress={pressSubmitAction}>
                             <Text style={css.submitText}>UPDATE</Text>
                         </TouchableOpacity>
                     </View>
@@ -79,6 +95,11 @@ const styles = StyleSheet.create({
         paddingTop: 36,
         paddingHorizontal: global.CONSTANTS.SIZE_20,
         alignItems: 'center',
+    },
+    closeButton: {
+        position: 'absolute',
+        top: global.CONSTANTS.SPACE_40,
+        right: global.CONSTANTS.SIZE_20,
     },
     buttonGroup: {
         flexDirection: 'row',
