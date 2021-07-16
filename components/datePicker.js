4eebeafc6b4bd196601a4c4fe253affa37bfa8f5
Loyal Lauzier <loyal@Loyals-Mac.local>
2021-07-16 06:39:34 -0700

    Fix an issue

--- components/datePicker.js
+++ components/datePicker.js
@@ -17,6 +17,13 @@ const DatePicker = (props) => {
         getNearestTime();
     }, []);
 
+    useEffect(() => {
+        if(selectedTime > 47)
+            setPM(true);
+        else
+            setPM(false);
+    }, [selectedTime]);
+
     const initStateValues = () => {
         selectDate(new Date());
         selectTime(12);
@@ -28,15 +35,6 @@ const DatePicker = (props) => {
         let yourTime = timeItems[selectedTime];
         let yourHour = parseInt(yourTime.split(':')[0]);
         let yourMin = parseInt(yourTime.split(':')[1]);
-        
-        if(isPM) {
-            yourHour += 12;
-            if(yourHour == 24)
-                yourHour = 12;
-        } else {
-            if(yourHour == 12)
-                yourHour = 0;
-        }
 
         yourDate.setHours(yourHour);
         yourDate.setMinutes(yourMin);
@@ -56,19 +54,19 @@ const DatePicker = (props) => {
         let dateString = (new Date()).toString();
         let timeString = dateString.split(' ')[4];
         timeString = timeString.slice(0, -3);
-        let hour = timeString.split(':')[0];
-        let min = timeString.split(':')[1];
-        let pm = Math.floor(hour / 12) > 0 ? true : false;
-        hour = hour % 12;
-        if(hour == 0)
-            hour = 12;
-        const idx = 4 * (hour - 1) + Math.ceil(min / 15);
-        if(idx > 47) {
-            setPM(!pm);
+        let hour = parseInt(timeString.split(':')[0]);
+        let min = parseInt(timeString.split(':')[1]);
+        
+        const idx = 4 * hour + Math.ceil(min / 15);
+        if(idx > 47)
+            setPM(true);
+        else
+            setPM(false);
+
+        if(idx > 95) {
             setSelIndex(0);
             selectTime(0);
         } else {
-            setPM(pm);
             setSelIndex(idx);
             selectTime(idx);
         }
@@ -125,11 +123,11 @@ const DatePicker = (props) => {
                                 defaultIndex={ selIndex }
                             />
                             <View style={styles.indicatorContainer}>
-                                <Pressable onPress={() => setPM(false)}>
+                                <Pressable>
                                     <Text style={[styles.indicatorText, { color: isPM ? global.COLOR.PRIMARY50 : global.COLOR.PRIMARY100 }]}>AM</Text>
                                 </Pressable>
                                 <Text style={css.modalTitleText}>  |  </Text>
-                                <Pressable onPress={() => setPM(true)}>
+                                <Pressable>
                                     <Text style={[styles.indicatorText, { color: isPM ? global.COLOR.PRIMARY100 : global.COLOR.PRIMARY50 }]}>PM</Text>
                                 </Pressable>
                             </View>
@@ -171,4 +169,28 @@ const styles = StyleSheet.create({
 
 export default React.memo(DatePicker);
 
-const timeItems = [ '1:00', '1:15', '1:30', '1:45', '2:00', '2:15', '2:30', '2:45', '3:00', '3:15', '3:30', '3:45', '4:00', '4:15', '4:30', '4:45', '5:00', '5:15', '5:30', '5:45', '6:00', '6:15', '6:30', '6:45', '7:00', '7:15', '7:30', '7:45', '8:00', '8:15', '8:30', '8:45', '9:00', '9:15', '9:30', '9:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45' ];
+const timeItems = [ '0:00', '0:15', '0:30', '0:45',
+                    '1:00', '1:15', '1:30', '1:45',
+                    '2:00', '2:15', '2:30', '2:45',
+                    '3:00', '3:15', '3:30', '3:45',
+                    '4:00', '4:15', '4:30', '4:45',
+                    '5:00', '5:15', '5:30', '5:45',
+                    '6:00', '6:15', '6:30', '6:45',
+                    '7:00', '7:15', '7:30', '7:45',
+                    '8:00', '8:15', '8:30', '8:45',
+                    '9:00', '9:15', '9:30', '9:45',
+                    '10:00', '10:15', '10:30', '10:45',
+                    '11:00', '11:15', '11:30', '11:45',
+                    '12:00', '12:15', '12:30', '12:45',
+                    '13:00', '13:15', '13:30', '13:45',
+                    '14:00', '14:15', '14:30', '14:45',
+                    '15:00', '15:15', '15:30', '15:45',
+                    '16:00', '16:15', '16:30', '16:45',
+                    '17:00', '17:15', '17:30', '17:45',
+                    '18:00', '18:15', '18:30', '18:45',
+                    '19:00', '19:15', '19:30', '19:45',
+                    '20:00', '20:15', '20:30', '20:45',
+                    '21:00', '21:15', '21:30', '21:45',
+                    '22:00', '22:15', '22:30', '22:45',
+                    '23:00', '23:15', '23:30', '23:45',
+                ];
