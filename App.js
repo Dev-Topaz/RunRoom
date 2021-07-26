414716a3270b0ba78dc4c5ede6296f69f30f2d25
Sophie <bolesalavb@gmail.com>
2021-07-25 18:28:37 -0700

    Remove unnecessary part

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppState } from 'react-native';
 import { useFonts } from 'expo-font';
 import { useAssets } from 'expo-asset';
