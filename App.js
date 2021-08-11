7486f808969bf24190d8795ad5903dd685bbb41b
Sophie <bolesalavb@gmail.com>
2021-08-11 05:40:59 -0700

    Fix an issue

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { AppState } from 'react-native';
 import { useFonts } from 'expo-font';
 import { useAssets } from 'expo-asset';
